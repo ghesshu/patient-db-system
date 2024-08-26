11a7d95bb127752c9180e17bfb57cda953568c55
ghesshu <hughestotimee@gmail.com>
2024-08-26 12:14:00 +0000

    update

--- constants/alt.ts
+++ constants/alt.ts
@@ -1,1 +1,10 @@
 export const UNI_URL = process.env.UNI_URL;
+
+// DB_URI="mongodb://localhost:27017"
+
+// EMAIL_USER = ""
+// EMAIL_PASS = ""
+
+// UNI_URL= "http://localhost:3000"
+
+// NEXTAUTH_SECRET="1234567890"
