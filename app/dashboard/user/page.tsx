24183c6c693548e9d7b40ec722707c6a5589f120
ghesshu <hughestotimee@gmail.com>
2024-08-27 05:57:26 +0000

    Update

--- app/dashboard/user/page.tsx
+++ app/dashboard/user/page.tsx
@@ -1,6 +1,6 @@
-import { Breadcrumbs } from '@/components/breadcrumbs';
+// import { Breadcrumbs } from '@/components/breadcrumbs';
 import PageContainer from '@/components/layout/page-container';
-import { UserClient } from '@/components/tables/user-tables/client';
+// import { UserClient } from '@/components/tables/user-tables/client';
 import { users } from '@/constants/data';
 
 const breadcrumbItems = [
@@ -11,8 +11,8 @@ export default function page() {
   return (
     <PageContainer>
       <div className="space-y-2">
-        <Breadcrumbs items={breadcrumbItems} />
-        <UserClient data={users} />
+        {/* <Breadcrumbs items={breadcrumbItems} /> */}
+        {/* <UserClient data={users} /> */}
       </div>
     </PageContainer>
   );
